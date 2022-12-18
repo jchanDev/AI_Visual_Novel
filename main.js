958407c19721219a106e67fb8c0ca76e3d65c419
Caleb Lee <caleb_lee@ucsb.edu>
2022-12-17 20:32:46 -0800

    working on dall-e image generation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import { Configuration, OpenAIApi } from 'openai';
 const diff_dialog = ["As Connor walked down the winding path, he couldn't help but feel a sense of unease wash over him. The trees towered over him, their branches rustling in the wind as if they were trying to communicate some secret message. Connor's heart pounded in his chest as he tried to steady his breathing. He had never been one for adventure, but he had agreed to come on this quest with his friends", "As he approached the clearing, he saw three figures standing before him. One was a tall, muscular man with piercing green eyes and a sly grin on his face. The second was a petite woman with long, flowing blonde hair and a kind expression. The third was a hooded figure, their face obscured by the shadows of their cloak", "Connor had no idea who these people were or what they wanted, but he knew he had to be cautious. He scanned the area for any signs of danger, but everything seemed peaceful. He took a deep breath and stepped forward, determined to find out the truth."];
 
 let index = 0;
@@ -22,3 +23,24 @@ dialogue_box.addEventListener('click', () => {
   }
 
 });
+
+
+const configuration = new Configuration({
+    apiKey: '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'
+});
+
+const openai = new OpenAIApi(configuration);
+
+const prompt = 'a hamster eating a peanut'
+
+const result = await openai.createImage({
+    prompt,
+    n: 1,
+    size: "1024x1024",
+});
+
+const url = result.data.data[0].url;
+var img = document.createElement("img");
+img.src = url;
+var src = document.getElementsByClassName("character");
+src.appendChild(img);
