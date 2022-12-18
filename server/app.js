e7c1fd444b0a4696e3facd1f165d46094850e212
juchan2021 <60370842+juchan2021@users.noreply.github.com>
2022-12-17 23:30:13 -0800

    added string slicing to seperate choices and text

--- server/app.js
+++ server/app.js
@@ -20,9 +20,14 @@ app.get('/start', async (req, res) => {
   //const result = await api.sendMessage("I want you to act as a visual novel narrator and its characters. My name is " + req.query.name + " and I will be the main character in the story, and I should be able to interact with the other characters. The narrator should refer to me using pronouns. The theme of the story will be: " + req.query.theme + " .You should only talk as the narrator in third person, or as the characters in first person. Before each of your responses, I want you to describe the physical appearance of the scene’s background using the format [BACKGROUND]: PHYSICAL APPEARANCE OF THE SCENE’S BACKGROUND. I want you to write character dialogue in the format, [NAME]: “DIALOGUE”. You must always describe the physical appearance of a character when they are speaking for the first time, using the format [NAME] (CHARACTER DESCRIPTION): “DIALOGUE”. I want you to give me 3 choices whenever the story needs to progress. The choices should be presented in the format of a numbered list. The narrator will ask me what choice I want to make, and you must wait for a response from the chat. The choice that I type in the chat will alter the storyline. The scene should change 5 times, and when a scene changes, write the text: [SCENE CHANGE]. You should prompt me with the choices at least one time per scene.")
   //const result = await api.sendMessage("I want you to act as a visual novel narrator and it's side charactors. I want you to set the scene and then only reply to me in conversation as characters. I want you to give me 4 choices everytime options are needed.")
   console.log(result.response)
+  let onlyText = result.response
+  let position = onlyText.search("1.")
+  let choiceText = onlyText.slice(position);
+  onlyText = onlyText.slice(0, position)
 
   res.send(JSON.stringify({
-    text: result.response,
+    text: onlyText,
+    choices: choiceText,
     conversationId: result.conversationId,
     messageId: result.messageId
   }))
@@ -34,12 +39,17 @@ app.get('/choose', async (req, res) => {
     parentMessageId: req.query.messageId
   })
   console.log(result.response)
+  let onlyText = result.response
+  let position = onlyText.search("1.")
+  let choiceText = onlyText.slice(position);
+  onlyText = onlyText.slice(0, position)
 
+  console.log(onlyText + '\n' + choiceText)
   res.send(JSON.stringify({
-    text: result.response,
+    text: onlyText,
+    choices: choiceText,
     conversationId: result.conversationId,
     messageId: result.messageId
-    //choices: result - choices
   }))
 })
 
